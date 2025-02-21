997767ebe860c7b553d16519c4340a9d73fef668
RasinthaDilshanJayarathne <rasinthadilshan9@gmail.com>
2025-02-21 11:02:42 +0530

    first commit

--- api/inngest/route.js
+++ api/inngest/route.js
@@ -1,10 +1,11 @@
 import { serve } from "inngest/next";
 import {
+import { Inngest } from 'inngest';
   inngest,
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
-} from "./../../config/inngest";
+} from "@/config/Inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
